47efa47bd7d548a3c82d53a72cc37834d690f381
Benji Schussheim <bschuss02@gmail.com>
2025-07-17 16:28:28 -0400

    redoing dataset

--- src/experiments/evaluate_asr.py
+++ src/experiments/evaluate_asr.py
@@ -13,7 +13,7 @@ import sys
 import time
 from dataclasses import asdict, dataclass
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional
 
 import hydra
 import numpy as np
