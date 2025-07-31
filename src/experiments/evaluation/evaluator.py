ab80b17deeeaac0683cf42ac076301dd2b729dea
Benji Schussheim <bschuss02@gmail.com>
2025-07-31 00:10:28 -0400

    Refactor evaluator to utilize ASRModelBase and EvaluationResult; enhance model loading and dataset evaluation methods for improved clarity and functionality

--- src/experiments/evaluation/evaluator.py
+++ src/experiments/evaluation/evaluator.py
@@ -3,12 +3,13 @@ from typing import List
 from zipfile import Path
 
 from experiments.config.EvaluationConfig import EvaluationConfig
-from experiments.utils.evaluation_result_row import EvaluationResultRow
+from experiments.inference_models.asr_model_base import ASRModelBase
+from experiments.utils.evaluation_result import EvaluationResult
 from experiments.utils.progress_manager import ProgressManager
 
 
 class Evaluator:
-    evaluation_results: List[EvaluationResultRow]
+    evaluation_results: List[EvaluationResult]
 
     def __init__(self, cfg: EvaluationConfig):
         self.cfg = cfg
@@ -19,21 +20,31 @@ class Evaluator:
         """Entrypoint for the evaluation process"""
         with ProgressManager() as progress:
             for model_name, model_path in self.cfg.models.items():
-                self._evaluate_model(model_name, model_path, progress)
+                model = self.load_model(model_name, model_path)
+                self._evaluate_model(model, progress)
                 progress.advance_model()
 
-    def _evaluate_model(self, progress: ProgressManager):
+    def _evaluate_model(self, model: ASRModelBase, progress: ProgressManager):
         """Iterate over all datasets and evaluate the model on each"""
         for dataset_name, dataset_path in self.cfg.datasets.items():
-            progress.start_dataset_processing(model_name, len(self.cfg.datasets))
-            self._evaluate_dataset(dataset_name, dataset_path, progress)
+            progress.start_dataset_processing(model.model_name, len(self.cfg.datasets))
+            self._evaluate_dataset(model, dataset_name, dataset_path, progress)
             progress.advance_dataset()
 
-    def _evaluate_dataset(self, progress: ProgressManager):
+    def _evaluate_dataset(
+        self,
+        model: ASRModelBase,
+        dataset_name: str,
+        dataset_path: Path,
+        progress: ProgressManager,
+    ):
         """Evaluate a single dataset. Called within _evaluate_model"""
+
+    def load_model(self, model_name: str, model_path: Path) -> ASRModelBase:
+        pass
 
     def _load_dataset(self, dataset_name: str, dataset_path: Path):
         pass
 
-    def _analyze_results(self, evaluation_results: List[EvaluationResultRow]):
+    def _analyze_results(self, evaluation_results: List[EvaluationResult]):
         pass
